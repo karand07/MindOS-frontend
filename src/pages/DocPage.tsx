bb06b8916d398f33856660b777bf216429f17bba
karand07 <karandanole07@gmail.com>
2026-03-02 20:08:51 +0530

    created photo  and blogs  pages

--- src/pages/DocPage.tsx
+++ src/pages/DocPage.tsx
@@ -1,9 +1,25 @@
-
+import { useContent } from "../hooks/useContent"
+import Card from "../components/Card" 
 
 function DocPage() {
-  return (
-    <div>DocPage</div>
-  )
-}
+  const { content, loading } = useContent("blog")
 
+  if (loading) return <div className="p-6">Loading...</div>
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Blog Content</h1>
+
+      <div className="flex gap-4 flex-wrap">
+        {content.map((item) => (
+          <Card
+            key={item._id}
+            title={item.title}
+            type={item.type}
+            link={item.link}
+          />
+        ))}
+      </div>
+    </div>)
+}
 export default DocPage
--- src/pages/PhotosPage.tsx
+++ src/pages/PhotosPage.tsx
@@ -1,7 +1,27 @@
-function PhotosPage() {
+import Card from "../components/Card"
+import { useContent } from "../hooks/useContent"
+
+function PhotoPage() {
+  const { content, loading } = useContent("photo")
+
+  if (loading) return <div className="p-6">Loading...</div>
+
   return (
-    <div>PhotosPage</div>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Photo Content</h1>
+
+      <div className="flex gap-4 flex-wrap">
+        {content.map((item) => (
+          <Card
+            key={item._id}
+            title={item.title}
+            type={item.type}
+            link={item.link}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default PhotosPage
+export default PhotoPage
