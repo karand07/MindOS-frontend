2fbdf278a935edd02b0e3384a986d2fd3ee36956
karand07 <karandanole07@gmail.com>
2026-03-16 12:53:52 +0530

    search bar added on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,22 +1,46 @@
+import { useState } from "react"
 import Card from "../components/Card"
 import { useContent } from "../hooks/useContent"
 
 function Dashboard() {
+
   const { content, loading } = useContent()
+  const [search, setSearch] = useState("")
 
   if (loading) return <div className="p-6">Loading...</div>
 
+  const filteredContent = content.filter((item) =>
+    item.title.toLowerCase().includes(search.toLowerCase()) ||
+    item.link.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div className="p-6 flex gap-4 flex-wrap">
-      {content.map((item) => (
-        <Card
-        _id={item._id}
-          key={item._id}
-          title={item.title}
-          type={item.type}
-          link={item.link}
-        />
-      ))}
+    <div className="p-6">
+
+      <div ><input
+        type="text"
+        placeholder="Search your brain..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="shadow border border-gray-300   rounded-md px-3 py-2 mb-6 w-full max-w-md"
+      /></div>
+      
+
+      <div className="flex gap-4 flex-wrap">
+        {filteredContent.length === 0 && (
+       <p className="text-gray-500">No results found</p>
+         )}
+        {filteredContent.map((item) => (
+          <Card
+            key={item._id}
+            _id={item._id}
+            title={item.title}
+            type={item.type}
+            link={item.link}
+          />
+        ))}
+      </div>
+
     </div>
   )
 }
